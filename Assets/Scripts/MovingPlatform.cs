414052e648dca25d1d2d9563d79d600ff0a432c2
Dragan Stamenkovic <draganstamenkovic92@hotmail.com>
2022-06-29 19:16:55 +0200

    Update moving platform
    * Destroy moving platform after it reaches 10 units in Y axis
    * added current platform difficulty variables for speeding up movements of platforms

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,16 +2,22 @@
 
 public class MovingPlatform : MonoBehaviour
 {
-    private static float speed = 0.2f;
-    private float currentDifficulty = 1f;
+    private static float speed = 2f;
+    private static float currentDifficulty = 1f;
     void Start()
     {
-
+        currentDifficulty = LevelManager.Instance.currentPlatformDifficulty;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.up * Time.deltaTime * speed * currentDifficulty;
+        if (transform.position.y > 10)
+        {
+            LevelManager.Instance.OnSpawnNext(transform.position.y);
+            Destroy(gameObject);
+        }
     }
+    
 }
